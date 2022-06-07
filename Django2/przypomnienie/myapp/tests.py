a4b971bd66decbd515280eb19dbd844d76be35c5
Alicja Pawlak <alicja.anna.pawlak@gmail.com>
2022-06-07 12:16:23 +0200

    nowe test_view

--- Django2/przypomnienie/myapp/tests.py
+++ Django2/przypomnienie/myapp/tests.py
@@ -1,14 +1,19 @@
-import html
-from django.http import HttpResponse
+from pydoc import resolve
+from urllib import response
 from django.test import TestCase
 from django.urls import resolve
 from myapp.views import home_page
+from selenium.webdriver.common.keys import Keys
+import time
 
 class HomePageTest(TestCase): 
 
+    def test_url(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
     def test_view(self): 
-        request = HttpResponse()
-        html = home_page(request)
-        self.assertTrue(html.startswith('<!DOCTYPE html>')) 
-        self.assertIn('<title>To-Do lists</title>', html) 
-        self.assertTrue(html.endswith('</html>'))
+        page = self.client.get('/', follow= True)
+        self.assertEqual(page.content.decode('utf8'), '<h1>Hello world</h1>')
+        self.assertEqual(page.status_code, 200)
+    
--- Django2/przypomnienie/myapp/views.py
+++ Django2/przypomnienie/myapp/views.py
@@ -1,9 +1,12 @@
 from typing import Any
 from urllib import response
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-def home_page(request):
 
-    html = request.content.decode('utf8')
-    return html
+def home_page(self):
+    html = '<h1>Hello world</h1>'
+    return HttpResponse(html)
+
+def post_form(self):
+    return HttpResponse()
